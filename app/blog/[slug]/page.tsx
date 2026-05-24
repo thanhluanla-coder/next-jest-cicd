6cff07c8dcda700d364ed2fa5b942b0e2be6bdf4
Luan La <ltluan@tma.com.vn>
2026-05-24 12:00:33 +0700

    update new

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata({ params }: Params) {
 
 export default function Page({ params }: Params) {
   return <h1>Slug: {params.slug} 
-  <p>Lunacy</p>
+  <p>LunacyCICD</p>
   <p>Welcome to HCM City</p>
   </h1>;
 }
